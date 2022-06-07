657f72a6448d4564cdc1afe0f99b3f70457a8422
SrdjanLakic <srdjan949@gmail.com>
2022-06-07 17:08:43 -0400

    player 1 logic implemented

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-document.addEventListener('DOMContentLoaded', () => {});
+document.addEventListener('DOMContentLoaded', () => {
+  const squares = document.querySelectorAll('.grid div');
+  const result = document.getElementById('result');
+  const displayCurrentPLayer = document.getElementById('current-player');
+  let currentPlayer = 1;
+  for (let i = 0; i < squares.length; i++) {
+    squares[i].onclick = () => {
+      if (squares[i + 7].classList.contains('taken')) {
+        if (currentPlayer === 1) {
+          squares[i].classList.add('taken');
+          squares[i].classList.add('player-one');
+          currentPlayer = 2;
+          displayCurrentPLayer.textContent = currentPlayer;
+        }
+      }
+    };
+  }
+});
